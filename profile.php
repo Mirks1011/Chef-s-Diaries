34328d098ce0402f04fa2562b50f2daf19d40337
Mirks1011 <laride.kurtjoel.avenido@gmail.com>
2024-11-30 15:31:32 +0800

    Add files via upload

--- profile.php
+++ profile.php
@@ -13,13 +13,97 @@ if (!isset($_SESSION['valid'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/profile.css">
+    <link rel="stylesheet" href="./Icons/boxicons-2.1.4/css/boxicons.min.css" />
+    <link rel="stylesheet" href="./Icons/fontawesome-free-6.6.0-web/css/all.css" />
     <script src="JS/popupform.js"></script>
     <title>Your Profile</title>
 </head>
 
 <body>
+
     <!---LOGO AND OVERALL--->
     <div class="content">
+        <!----START OF SIDEBAR---->
+        <nav class="sidebar close">
+            <header>
+                <div class="image-text">
+                    <span class="image">
+                        <img src="Temp logo.png" alt="Group 7's Logo" />
+                    </span>
+
+                    <div class="text header-text">
+                        <span class="name"> CHEF'S Corner</span>
+                    </div>
+                </div>
+                <i class="bx bxs-chevrons-right toggle"></i>
+            </header>
+            <div class="menu-bar">
+                <div class="menu">
+                    <li class="search-box">
+                        <i class="bx bx-search icons"></i>
+                        <input type="search" placeholder="What ya lookin' for?" />
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="userindex.php">
+                            <i class="bx bx-home-smile icons"></i>
+                            <span class="text nav-text">Home</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="recipe.html">
+                            <i class="fa-solid fa-utensils icons"></i>
+                            <span class="text nav-text">Recipe</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="recipe.html">
+                            <i class="fa-solid fa-video icons"></i>
+                            <span class="text nav-text">Video Tutorials</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="planner.php">
+                            <i class="fa-regular fa-calendar-check icons"></i>
+                            <span class="text nav-text">Planner</span>
+                        </a>
+                    </li>
+                </div>
+                <div class="bottom-content">
+                    <li class="">
+                        <a href="profile.php">
+                            <i class="fa-solid fa-user icons"></i>
+                            <span class="text nav-text">Your Profile</span>
+                        </a>
+
+                    <li class="logout">
+                        <a href="php/logout.php">
+                            <i class="fa-solid fa-arrow-left icons"></i>
+                            <span class="text nav-text">Log Out</span>
+                        </a>
+
+                    </li>
+                    <li class="mode">
+                        <div class="moon-sun-icon">
+                            <i class="fa-solid fa-moon icons moon"></i>
+                            <i class="fa-solid fa-sun icons sun"></i>
+                        </div>
+                        <p class="text mode-text">Dark Mode</p>
+
+                        <div class="toggle-switch">
+                            <span class="switch"></span>
+                        </div>
+                    </li>
+                </div>
+            </div>
+        </nav>
+
+        <!----------SIDEBAR SCRIPT W/DARK---------->
+        <script src="JS/profile.js"></script>
+        <!-----END OF SIDEBAR----->
         <div class="nav">
             <div class="logo">
                 <img src="Logo/CHEF_S_DIARIES-removebg-preview.png" alt="Logo of Chef's diaries">
